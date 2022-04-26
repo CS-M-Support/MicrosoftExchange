65c06489a7a5c6476a059597391f020737aa4d97
Johnny Winkler <johnnyf.winkler@hotmail.com>
2022-04-26 09:30:04 +0200

    Create freigegebene (geister) postfächer löschen.ps1

---
+++ Exchange Online/freigegebene (geister) postfächer löschen.ps1
@@ -0,0 +1,11 @@
+# Mit ExchangeOnline verbinden
+Connect-ExchangeOnline
+
+# Übersicht aller freigegebenen Postfächer (auch wenn sie nicht im Exchange Admin Center angezeigt werden)
+Get-MailPublicFolder
+
+# Löschen / Deaktivieren eines freigegebenen Postfach
+Get-MailPublicFolder [Name of the Public Folder] | Disable-MailPublicFolder
+
+# Verbindung mit Exchange Online trennen
+Disconnect-ExchangeOnline
