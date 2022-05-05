8881a48eae26de169845e37962d6d8357e68d63e
Johnny Winkler <johnnyf.winkler@hotmail.com>
2022-05-05 12:02:46 +0200

    Update import starten.ps1

--- Exchange Online/Kontakte in GAL importieren/import starten.ps1
+++ Exchange Online/Kontakte in GAL importieren/import starten.ps1
@@ -1,4 +1,4 @@
-# Mit Online Dienst verbinden
+# Verbindung mit Exchange Online herstellen
 Connect-ExchangeOnline
 
 # CSV Datei muss mit dem Namen "Kontakte" im UTF-8 Format im Downloads Ordner abgespeichert sein
@@ -6,3 +6,6 @@ cd C:\Users\$env:username\Downloads
 Import-Csv .\Kontakte.csv|%{New-MailContact -Name $_.DisplayName -DisplayName $_.DisplayName -ExternalEmailAddress $_.Email -FirstName $_.FirstName -LastName $_.LastName}
 $contacts = Import-CSV .\Kontakte.csv
 $contacts | ForEach {Set-Contact $_.Name -StreetAddress $_.StreetAddress -City $_.City -StateorProvince $_.Region -PostalCode $_.ZIP -Phone $_.CompanyPhone -MobilePhone $_.MobilePhone -Company $_.Company -Fax $_.FAX}
+
+# Verbindung mit Exchange Online trennen
+Disconnect-ExchangeOnline
