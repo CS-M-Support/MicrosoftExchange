449c431c7f276a3605f1c061cd87ec5c0d840cf0
Johnny Winkler <johnnyf.winkler@hotmail.com>
2022-05-05 12:02:41 +0200

    Update kalender berechtigungen anpassen.ps1

--- Exchange Online/kalender berechtigungen anpassen.ps1
+++ Exchange Online/kalender berechtigungen anpassen.ps1
@@ -1,5 +1,17 @@
 # Mit ExchangeOnline Verbinden
 Connect-ExchangeOnline
 
-# Berechtigungen setzen
+# Neue Berechtigungen setzen
 Add-MailboxFolderPermission -Identity [UPN]:\Calendar -User [UPN] -AccessRights [PermissionLevel]
+
+# Bestehende Berechtigungen anpassen
+Set-MailboxFolderPermission -Identity [UPN]:\Calendar -User [UPN] -AccessRights [PermissionLevel]
+
+# Author:             CreateItems, DeleteOwnedItems, EditOwnedItems, FolderVisible, ReadItems
+# Contributor:        CreateItems, FolderVisible
+# Editor:             CreateItems, DeleteAllItems, DeleteOwnedItems, EditAllItems, EditOwnedItems, FolderVisible, ReadItems
+# NonEditingAuthor:   CreateItems, DeleteOwnedItems, FolderVisible, ReadItems
+# Owner:              CreateItems, CreateSubfolders, DeleteAllItems, DeleteOwnedItems, EditAllItems, EditOwnedItems, FolderContact, FolderOwner, FolderVisible, ReadItems
+# PublishingAuthor:   CreateItems, CreateSubfolders, DeleteOwnedItems, EditOwnedItems, FolderVisible, ReadItems
+# PublishingEditor:   CreateItems, CreateSubfolders, DeleteAllItems, DeleteOwnedItems, EditAllItems, EditOwnedItems, FolderVisible, ReadItems
+# Reviewer:           FolderVisible, ReadItems
