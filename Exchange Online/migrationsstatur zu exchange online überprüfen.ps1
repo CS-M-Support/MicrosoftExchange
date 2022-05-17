51159f32192389b53aab8b5d771f915153eb3f99
Johnny Winkler <johnnyf.winkler@hotmail.com>
2022-05-17 11:46:53 +0200

    Delete migrationsstatur zu exchange online überprüfen.ps1

--- Exchange Online/migrationsstatur zu exchange online überprüfen.ps1
+++
@@ -1,8 +0,0 @@
-# Verbindung mit Exchange Online herstellen
-Connect-ExchangeOnline
-
-# Abfrage der Statusübersicht der Migrationen
-Get-MoveRequest
-
-# Verbindung mit Exchange Online trennen
-Disconnect-ExchangeOnline
