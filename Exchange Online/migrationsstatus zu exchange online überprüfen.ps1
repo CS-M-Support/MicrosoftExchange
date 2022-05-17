30066b51ac73a710ef42b84560079b4752f21fd4
Johnny Winkler <johnnyf.winkler@hotmail.com>
2022-05-17 11:46:58 +0200

    Create migrationsstatus zu exchange online überprüfen.ps1

---
+++ Exchange Online/migrationsstatus zu exchange online überprüfen.ps1
@@ -0,0 +1,8 @@
+# Verbindung mit Exchange Online herstellen
+Connect-ExchangeOnline
+
+# Abfrage der Statusübersicht der Migrationen
+Get-MoveRequest
+
+# Verbindung mit Exchange Online trennen
+Disconnect-ExchangeOnline
