cde3aca265e54c1d30dbf6434307a398879a0706
Johnny Winkler <johnnyf.winkler@hotmail.com>
2022-05-25 10:59:59 +0200

    Update kalender berechtigungen anpassen.ps1

--- Exchange Online/kalender berechtigungen anpassen.ps1
+++ Exchange Online/kalender berechtigungen anpassen.ps1
@@ -1,6 +1,9 @@
 # Verbindung mit Exchange Online herstellen
 Connect-ExchangeOnline
 
+# Aktuelle Berechtigungen auslesen
+Get-EXOMailboxFolderPermission -Identity [UPN]:\Calendar
+
 # Neue Berechtigungen setzen
 Add-MailboxFolderPermission -Identity [UPN]:\Calendar -User [UPN] -AccessRights [PermissionLevel]
 
@@ -15,6 +18,9 @@ Set-MailboxFolderPermission -Identity [UPN]:\Calendar -User [UPN] -AccessRights 
 # PublishingAuthor:   CreateItems, CreateSubfolders, DeleteOwnedItems, EditOwnedItems, FolderVisible, ReadItems
 # PublishingEditor:   CreateItems, CreateSubfolders, DeleteAllItems, DeleteOwnedItems, EditAllItems, EditOwnedItems, FolderVisible, ReadItems
 # Reviewer:           FolderVisible, ReadItems
+
+# Wichtige Info:
+# Sollte \Calendar nicht funktionieren, kann man es mit \Kalender versuchen. Teilweise funktioniert nur der deutsche Ordnername.
 
 # Verbindung mit Exchange Online trennen
 Disconnect-ExchangeOnline
